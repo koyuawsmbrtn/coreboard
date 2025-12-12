d0cc7a91e28fb944ce5b8e825bdbce944c045a9d
koyu <me@koyu.space>
2025-12-12 08:42:41 +0100

    feat: update Sanity configuration in .env.example and refactor sanityConnection to use environment variables

--- packages/sanity-connection/index.ts
+++ packages/sanity-connection/index.ts
@@ -1,9 +1,11 @@
+import { PUBLIC_SANITY_PAGE_TITLE, PUBLIC_SANITY_PUBLIC_VIEWER_TOKEN, PUBLIC_SANITY_STUDIO_HOST, PUBLIC_SANITY_STUDIO_URL, PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_PREVIEW_URL, PUBLIC_SANITY_DATASET } from '$env/static/public';
+
 export const sanityConnection = {
-  pageTitle: "Coreboard",
-  publicViewerToken: "skfYYEuLCRMcMWjbtwmc25xhwlPTcKA0Ntik8VGw4JPjaJWtodZqaxvPmQ0tGZfTuWDkagTpsX0gIObkxC9YmAoGbl15L4OU9AuKaACzMAGWnxZ6rQ4a6GpczH8yZEPbE2Q508gtMODTGG2dysIbtvewtnqY0E8pAkTEzRStoxuokWgNl9G6",
-  studioHost: "coreboard",
-  studioUrl: "https://coreboard.sanity.studio",
-  projectId: "u3x2jw5m",
-  previewUrl: "http://localhost:5173",
-  dataset: "production",
+  pageTitle: PUBLIC_SANITY_PAGE_TITLE || "Coreboard",
+  publicViewerToken: PUBLIC_SANITY_PUBLIC_VIEWER_TOKEN || "skfYYEuLCRMcMWjbtwmc25xhwlPTcKA0Ntik8VGw4JPjaJWtodZqaxvPmQ0tGZfTuWDkagTpsX0gIObkxC9YmAoGbl15L4OU9AuKaACzMAGWnxZ6rQ4a6GpczH8yZEPbE2Q508gtMODTGG2dysIbtvewtnqY0E8pAkTEzRStoxuokWgNl9G6",
+  studioHost: PUBLIC_SANITY_STUDIO_HOST || "coreboard",
+  studioUrl: PUBLIC_SANITY_STUDIO_URL || "https://coreboard.sanity.studio",
+  projectId: PUBLIC_SANITY_PROJECT_ID || "u3x2jw5m",
+  previewUrl: PUBLIC_SANITY_PREVIEW_URL || "http://localhost:5173",
+  dataset: PUBLIC_SANITY_DATASET || "production",
 };
